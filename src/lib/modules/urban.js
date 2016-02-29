ac5d1f56a233cfd0e7682fea392ce74c906a74e2
Ghost1227 <dgriffiths@section214.com>
2016-02-29 02:33:08 -0600

    Add Urban Dictionary module

---
+++ src/lib/modules/urban.js
@@ -0,0 +1,46 @@
+/**
+ * Urban Dictionary Module
+ *
+ * @author      Daniel J Griffiths <dgriffiths@section214.com>
+ * @license     GPL-2.0
+ * @since       0.1.6
+ */
+
+'use strict';
+
+
+/**
+ * Urban Dictionary Module class
+ *
+ * @since       0.1.6
+ */
+class urban {
+
+
+    /**
+     * Get an Urban Dictionary definition
+     *
+     * @since       0.1.6
+     * @access      public
+     * @param       {object} res The message resource
+     * @param       {string} arguements The command arguements
+     * @return      {void}
+     */
+    _urban(res, arguement) {
+        let urban = require('urban');
+        let utils = require(GLOBAL.k9path + '/lib/core/utils.js');
+
+        if(! arguement || arguement === ' ') {
+            arguement = 'moron';
+        }
+
+        let response = urban(arguement);
+
+        response.first(function(json) {
+            utils.reply(res, "\"" + json.word + "\" is defined as \"" + json.definition + "\"\nExample: \"" + json.example + "\"");
+            return;
+        });
+    }
+}
+
+module.exports = new urban();
