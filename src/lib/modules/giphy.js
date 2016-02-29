e079b900bb140d669fe5995bfddbaa9ce6100192
Ghost1227 <dgriffiths@section214.com>
2016-02-29 01:55:02 -0600

    Add Giphy module

---
+++ src/lib/modules/giphy.js
@@ -0,0 +1,56 @@
+/**
+ * Giphy Module
+ *
+ * @author      Daniel J Griffiths <dgriffiths@section214.com>
+ * @license     GPL-2.0
+ * @since       0.1.6
+ */
+
+'use strict';
+
+
+/**
+ * Giphy Module class
+ *
+ * @since       0.1.6
+ */
+class giphy {
+
+
+    /**
+     * Get a random gif
+     *
+     * @since       0.1.6
+     * @access      public
+     * @param       {object} res The message resource
+     * @param       {string} arguements The command arguements
+     * @return      {void}
+     */
+    _giphy(res, arguement) {
+        let utils   = require(GLOBAL.k9path + '/lib/core/utils.js');
+        let config  = require(GLOBAL.k9path + '/lib/core/config.js');
+        let api_key = config.get('giphy_api_key', '');
+        let rating  = config.get('giphy_rating', false);
+        let giphy   = require('giphy-api')(api_key);
+
+        if(! arguement || arguement === ' ') {
+            arguement = false;
+        }
+
+        giphy.random({
+            tag: arguement,
+            rating: rating,
+            fmt: 'json'
+        }, function(err, img) {
+            if(err) {
+                utils.reply(res, "Oh my Giphy! Something went wrong...");
+                return;
+            }
+
+            utils.reply(res, img.data.image_url);
+            return;
+        });
+    }
+}
+
+module.exports = new giphy();
